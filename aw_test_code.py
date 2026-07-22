0a6405c853c05f041f01f99bfcc5a619183f2dfc
Angie Wang <wangie@stanford.edu>
2026-07-21 18:11:46 -0700

    added the calculation for six

--- aw_test_code.py
+++ aw_test_code.py
@@ -6,4 +6,4 @@ print("Two is", (4*4)/(4+4))
 print("Three is", (4+4+4)/4)
 print("Four is", sqrt(4*4)/(4/4))
 print("Five is", sqrt(4)+sqrt(4)+(4/4))
-
+print("Six is", 4+(4+4)/4) # this line is new
